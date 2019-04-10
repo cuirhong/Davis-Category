7608fc59ef56e82a4593062756652357ff9f82d2
davis <cuirhong@126.com>
2019-04-10 18:49:23 +0800

    init

---
+++ Source/CocoapodsTestFile.swift
@@ -0,0 +1,12 @@
+//
+//  CocoapodsTestFile.swift
+//  OurVillages
+//
+//  Created by cuirhong on 2019/4/10.
+//  Copyright © 2019 深圳市昊昱显信息技术有限公司. All rights reserved.
+//
+
+import Foundation
+class CocoapodsTestFile {
+     
+}
